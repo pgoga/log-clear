14b24d0303cc10ac8b0d29f1627fdd8f369770da
George P <george.plehanov@gmail.com>
2018-12-12 00:40:06 +0900

    fixed

--- delete.sh
+++ delete.sh
@@ -1,3 +1,3 @@
-gcloud auth activate-service-account --key-file $1.json
+gcloud auth activate-service-account --key-file /usr/app/iam/$1.json
 gcloud config set project $1
 for i in $(gcloud beta logging logs list); do gcloud beta logging logs delete $i --quiet; done
